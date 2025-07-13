10273dc1efcd1f72e0994d3f665a98a2ca7d5747
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-13 16:48:53 -0300

    operadores aritmeticos

---
+++ Clase 2/aritmeticos.py
@@ -0,0 +1,26 @@
+'''
+Operaciones aritméticas básicas en Python.
+suma, resta, multiplicación y división.
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b   # División que siempre devuelve un float
+h = a % b   # Sirve para obtener el residuo de la división
+i = a ** b  # Potencia
+'''
+a = 17
+b = 18 
+c = a + b
+d = a - b
+e = a * b
+f = a / b   # División que siempre devuelve un float
+h = a % b   # Sirve para obtener el residuo de la división
+i = a ** b  # Potencia
+print("Suma:", c)
+print("Resta:", d)
+print("Multiplicación:", e)
+print("División:", f)
+print("Residuo de la división:", h)
+print("Potencia:", i)
+print("Fin del programa")
