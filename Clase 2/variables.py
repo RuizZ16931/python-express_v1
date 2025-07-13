e51ae207ee11a7ee6898bd7e09a3d6d4fb68a0d2
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-13 16:02:44 -0300

    finalizado variables

--- Clase 2/variables.py
+++ Clase 2/variables.py
@@ -1,1 +1,33 @@
-print("Hola mundo")
+
+'''Sunt aliqua velit reprehenderit adipisicing cupidatat consectetur consequat. Nisi magna esse consequat nisi ut commodo. Officia voluptate aliqua ad 
+laborum cupidatat Lorem labore aliqua nostrud mollit reprehenderit. 
+Ex occaecat mollit consectetur consectetur enim ipsum anim reprehenderit irure elit laboris adipisicing pariatur.
+Esse sit nulla ut consectetur consectetur deserunt ut.'''
+
+
+print("Hola Mundo desde la unida")
+
+entero = 10
+flotante = 10.5
+cadena = "Hola Python"
+booleano = True
+print(entero, flotante, cadena, booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+
+print("Sumando dos numeros")
+# Operaciones aritmeticas
+# Suma, Resta, Multiplicacion, Division
+a = 5
+b = 10
+suma = a + b
+print("La suma de", a, "y", b, "es:", suma)
+
+print("Restando dos numeros")
+print("La resta de", a, "y", b, "es:", a - b)
+
+print("Multiplicando dos numeros")
+print("La multiplicacion de", a, "y", b, "es:", a*b)
+
+b = 2
+print("Dividiendo dos numeros")
+print("La division de", a, "y", b, "es:", a/b)  
