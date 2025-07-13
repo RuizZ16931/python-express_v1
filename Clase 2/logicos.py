6780ac7f09749d46f59742707053b7d7243e0709
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-13 18:16:47 -0300

    logicos

---
+++ Clase 2/logicos.py
@@ -0,0 +1,11 @@
+'''
+and Sirve para evaluar si dos condiciones son verdaderas.
+or Sirve para evaluar si al menos una de las condiciones es verdadera.
+not Sirve para invertir el valor de una condición.
+
+'''
+edad= 17
+if(edad >= 0 or edad <= 18):
+    print("Menor de edad")
+else:
+    print("Mayor de edad")
