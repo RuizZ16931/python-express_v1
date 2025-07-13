c938a898b576fd64fab6accc45e8ea79e088ebf4
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-13 16:15:56 -0300

    finalizado operadores

---
+++ Clase 2/operadores.py
@@ -0,0 +1,24 @@
+'''
+Operadores de Comparación
+Estos operadores se utilizan para comparar dos valores y devuelven un valor booleano 
+(True o False) según el resultado de la comparación.
+a==b Significa "a es igual a b"
+a!=b Significa "a es diferente de b"
+a < b Significa "a es menor que b"
+a > b Significa "a es mayor que b"
+a <= b Significa "a es menor o igual que b"
+a >= b Significa "a es mayor o igual que b"
+'''
+r = 100 == 100
+print(r)  # True
+r = 100 != 100
+print(r)  # False
+r = 100 < 100
+print(r)  # False
+r = 100 > 100
+print(r)  # False
+r = 100 <= 100
+print(r)  # True
+r = 100 >= 100
+print(r)  # True
+
