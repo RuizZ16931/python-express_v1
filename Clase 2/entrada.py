8e2f39806b782fb128ec5f1ca8f5476a21278ec7
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-23 20:00:15 -0300

    entrada por teclado

---
+++ Clase 2/entrada.py
@@ -0,0 +1,12 @@
+'''
+Entrada de Datos por teclado
+
+'''
+
+# Etrada de datos por teclado
+cadena = input("Ingrese nombre de su proyecto: ")
+print(f"El nombre de su proyecto es: {cadena}")
+
+numero = int(input("Que version es tu proyecto: "))
+print(f"La version de tu proyecto es: {numero+1}")
+
