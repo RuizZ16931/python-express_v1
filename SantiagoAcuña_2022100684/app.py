a73140de6a9d94710faa7ff86d5ad0c49f941f44
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-24 19:30:53 -0300

    Examen_Final

--- SantiagoAcuña_2022100684/app.py
+++ SantiagoAcuña_2022100684/app.py
@@ -12,9 +12,7 @@ app.register_blueprint(logout)
 
 @app.route('/', methods=['GET'])
 def unida():
-    return 'Server is running on port 5000!'
+    return 'Server is running on port 5003!'
 
 if __name__ == '__main__':
-    # The 0.0.0.0 host allows the application to be accessible from any IP address
-    # El 0.0.0.0 permite que la aplicación sea accesible desde cualquier dirección IP
-    app.run(debug=True, port=5000, host='0.0.0.0') # Cambia el puerto y host según sea necesario
+    app.run(debug=True, port=5003, host=localhost) 
