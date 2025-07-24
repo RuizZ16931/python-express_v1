14365b89fd31849c2ddb8a302e032d1613f43963
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-23 23:05:07 -0300

    funciones

---
+++ Clase 2/funciones.py
@@ -0,0 +1,24 @@
+"""
+Ejemplos de funciones en Python.
+
+"""
+
+a = 5
+b = 10
+
+def suma(a, b):
+    return a + b
+
+resultado = suma(a, b)
+print("La suma de 5 y 10 es:", resultado)
+
+def resta(a, b):
+    return a - b
+
+resultado = resta(a, b)
+print("La resta de 10 y 5 es:", resultado)
+
+def multiplicacion(a, b):
+    return a * b
+resultado = multiplicacion(a, b)
+print("La multiplicacion de 5 y 10 es:", resultado)
