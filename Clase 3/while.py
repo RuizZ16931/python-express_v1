cf97002b041c0e72eeda2bcfb0efdf01426785df
Santiago German Ruiz Diaz <ruizdisantiago016@gmail.com>
2025-07-23 21:40:30 -0300

    while

---
+++ Clase 3/while.py
@@ -0,0 +1,11 @@
+"""
+Estructura de control while
+"""
+
+contador = 0 # Inicializamos el contador
+while contador < 10:    # Repetimos mientras contador sea menor que 10
+    print("El contador es:", contador) # Imprimimos el valor del contador
+    contador += 1 # Incrementamos el contador en 1
+print("Fin del bucle while")
+
+
